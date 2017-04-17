d8996f0c86691f59366d0e545c93431707c794d5
Nikhil Gurnani <niksgurnani@live.com>
2017-04-18 01:14:08 +0530

    Displayed Username with Blogs in Blog Loader.

--- WTProject/BlogLoader.aspx.cs
+++ WTProject/BlogLoader.aspx.cs
@@ -11,8 +11,6 @@ namespace WTProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-
             if (!Page.IsPostBack)
             {
                 DBInteractiobDataContext dc = new DBInteractiobDataContext();
@@ -24,7 +22,11 @@ namespace WTProject
 
                 foreach (var post in posts)
                 {
-                    PostArea.InnerHtml = PostArea.InnerHtml + $"<div class='col-md-8'><div class='post-item wow' style='padding:10px;' data-animation='fadeInUp' data-animation-delay='300'> <br><h2 style='padding-top:20px;' class='wow fadeInUp'><a href = 'BlogPage.aspx?id={post.postsid}'>{post.posttitle}</a></h2> <div class='post wow fadeInUp'> <a href = 'BlogPage.aspx?id={post.postsid}'><img height='350px' width='745px' src='images/blog/{post.headerimage}' alt='blog /></a> <div class='post-content'><p>..{post.writtencontent.Substring(post.writtencontent.Length - 360)}</p> <div class='posted-date'>July 19, 2014   /   <span>by</span> <a href = '#' >{post.userid}</a> </div></div></div></div>";
+                    var u = from n in dc.Users
+                            where n.userid == post.userid
+                            select n;
+                    User user = (User)u.First();
+                    PostArea.InnerHtml = PostArea.InnerHtml + $"<div class='col-md-8'><div class='post-item wow' style='padding:10px;' data-animation='fadeInUp' data-animation-delay='300'> <br><h2 style='padding-top:20px;' class='wow fadeInUp'><a href = 'BlogPage.aspx?id={post.postsid}'>{post.posttitle}</a></h2> <div class='post wow fadeInUp'> <a href = 'BlogPage.aspx?id={post.postsid}'><img height='350px' width='745px' src='images/blog/{post.headerimage}' alt='blog /></a> <div class='post-content'><p>..{post.writtencontent.Substring(post.writtencontent.Length - 360)}</p> <div class='posted-date'>{post.date_added}   /   <span>by</span> <a href = '#' >{user.name}</a> </div></div></div></div>";
                 }
             }
         }
